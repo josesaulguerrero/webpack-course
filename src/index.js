c36330e14ad75a4fc95b19d3831151566db9c244
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2021-07-03 16:16:05 -0500

    fix v2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 // import './styles/vars.styl';
 // import './styles/vars.scss';
-console.log('hola')
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
